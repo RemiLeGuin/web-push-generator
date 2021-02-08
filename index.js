3c1e7463e3e86d4a0712cd96d90e7362477eda78
Rémi Le Guin <remileguin@live.fr>
2021-02-08 10:19:23 +0100

    Final

--- index.js
+++ index.js
@@ -23,13 +23,6 @@ app.get('/test', (req, res) => {
 });
 
 app.post("/sendNotification", (req, res) => {
-    /*var endpoint = "https://fcm.googleapis.com/fcm/send/d8NLt3q2YVw:APA91bE9B4zVmw5kNyBznOzUZhqcPI_XeP4M2pkR0O2FrmoLLoSpVgcBD7LaKwTab9e5QT9tyrjx1H9zYChegQ-w7J_jQpNqhIcnC7IEomchRXOwPsSRK4M-9fkB2SAS4TE8ZMhwrf5L";
-    var p256dh = "BE8FTLMvg8l1H4TKbzp2yc0bFiknFzVspLc4mAhJOmOA4N0jy7kYJFkXHlnN86Lcja8jsMxIJOdXOFDlktBodms";
-    var auth = "sCvVu4sAaBI7FFTdAV2Uhg";
-    
-    var vapidPublicKey = "BEcjZx6myA9LIg11zU6zl88sj1KLSiI-nVePDHY6gYjKNmUGIQTF94UKMzsgctV6YKtbPIek4c2VLXOyLzwgCV0";
-    var vapidPrivateKey = "gBVTFi4DQeV4XyfK2cq_WWQbbZ2oWPNLhmY5eDOixYg";*/
-
     var endpoint = req.body.endpoint;
     var p256dh = req.body.p256dh;
     var auth = req.body.auth;
@@ -46,7 +39,7 @@ app.post("/sendNotification", (req, res) => {
         },
     };
 
-    var payload = "Coucou twa!";
+    var payload = req.body.payload;
 
     var options = {
         vapidDetails: {
