83e97b73157223b655f94ea301a5e6e302d9aa25
Rémi Le Guin <remileguin@live.fr>
2021-03-01 12:03:48 +0100

    Mass notifications support

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const compression = require('compression');
-const helmet = require('helmet');
+const compression = require("compression");
+const helmet = require("helmet");
 var express = require("express");
 var webPush = require("web-push");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 var app = express();
 app.use(helmet());
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3002;
-const DIST_DIR = './dist';
+const DIST_DIR = "./dist";
 app.use(express.static(DIST_DIR));
 
 app.listen(PORT, () => {
@@ -48,4 +48,32 @@ app.post("/sendNotification", (req, res) => {
 
     webPush.sendNotification(pushSubscription, payload, options);
     res.status(201).send('Notification OK');
+});
+
+app.post("/sendNotifications", (req, res) => {
+    const subscriptions = req.body.subscriptions;
+    const payloads = req.body.payloads;
+    const options = {
+        vapidDetails: {
+            subject: "mailto:remileguin@live.fr",
+            publicKey: req.body.vapidPublicKey,
+            privateKey: req.body.vapidPrivateKey,
+        },
+        TTL: 60,
+    };
+
+    payloads.forEach((payload) => {
+        subscriptions.forEach((subscription) => {
+            var pushSubscription = {
+                endpoint: subscription.endpoint,
+                expirationTime: null,
+                keys: {
+                    p256dh: subscription.p256dh,
+                    auth: subscription.auth,
+                },
+            };
+            webPush.sendNotification(pushSubscription, payload, options);
+        });
+    });
+    res.status(201).send('Notifications sent');
 });
