72676311fc9b0f89923c19651c26ab2ae8319b77
Rémi Le Guin <remileguin@live.fr>
2021-03-16 21:51:44 +0100

    Multiple optimizations for stable version

--- index.js
+++ index.js
@@ -1,72 +1,32 @@
-const compression = require("compression");
-const helmet = require("helmet");
-var express = require("express");
-var webPush = require("web-push");
-const bodyParser = require("body-parser");
+const compression = require('compression');
+const helmet = require('helmet');
+var express = require('express');
+var webPush = require('web-push');
 var app = express();
 app.use(helmet());
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3002;
-const DIST_DIR = "./dist";
+const DIST_DIR = './dist';
 app.use(express.static(DIST_DIR));
 
 app.listen(PORT, () => {
-    console.log("Server running on port 3000");
+    console.log(
+        `✅  API Server started: http://${HOST}:${PORT}`
+    )
 });
 
-app.post("/sendNotification", (req, res) => {
-    var endpoint = req.body.endpoint;
-    var p256dh = req.body.p256dh;
-    var auth = req.body.auth;
-
-    var vapidPublicKey = req.body.vapidPublicKey;
-    var vapidPrivateKey = req.body.vapidPrivateKey;
-
-    var pushSubscription = {
-        endpoint: endpoint,
-        expirationTime: null,
-        keys: {
-            p256dh: p256dh,
-            auth: auth,
-        },
-    };
-
-    var payload = req.body.payload;
-
-    var options = {
-        vapidDetails: {
-            subject: "mailto:remileguin@live.fr",
-            publicKey: vapidPublicKey,
-            privateKey: vapidPrivateKey,
-        },
-        TTL: 60,
-    };
-
-    webPush.sendNotification(pushSubscription, payload, options);
-    res.status(201).send('Notification OK');
-});
-
-app.post("/sendNotifications", (req, res) => {
-    const subscriptions = req.body.subscriptions;
+app.post('/sendNotifications', (req, res) => {
     const payloads = req.body.payloads;
-    const options = {
-        vapidDetails: {
-            subject: "mailto:remileguin@live.fr",
-            publicKey: req.body.vapidPublicKey,
-            privateKey: req.body.vapidPrivateKey,
-        },
-        TTL: 60,
-    };
-
-    webPush.setVapidDetails("mailto:remileguin@live.fr", req.body.vapidPublicKey, req.body.vapidPrivateKey);
+    const subscriptions = req.body.subscriptions;
+    webPush.setVapidDetails('mailto:remileguin@live.fr', req.body.vapidPublicKey, req.body.vapidPrivateKey);
 
     payloads.forEach((payload) => {
         subscriptions.forEach((subscription) => {
-            var pushSubscription = {
+            let pushSubscription = {
                 endpoint: subscription.endpoint,
                 expirationTime: null,
                 keys: {
