53d14de42fca6d488ae25caca6dd6c18444dde74
Rémi Le Guin <remileguin@live.fr>
2021-03-16 08:44:19 +0100

    Debug

--- index.js
+++ index.js
@@ -85,24 +85,33 @@ app.post("/sendNotifications", (req, res) => {
 });
 */
 app.post("/sendNotifications", (req, res) => {
-    const subscription = req.body.subscriptions[0];
-    const payload = req.body.payloads[0];
-    const options = {
+    var endpoint = req.body.subscriptions[0].endpoint;
+    var p256dh = req.body.subscriptions[0].p256dh;
+    var auth = req.body.subscriptions[0].auth;
+
+    var vapidPublicKey = req.body.vapidPublicKey;
+    var vapidPrivateKey = req.body.vapidPrivateKey;
+
+    var pushSubscription = {
+        endpoint: endpoint,
+        expirationTime: null,
+        keys: {
+            p256dh: p256dh,
+            auth: auth,
+        },
+    };
+
+    var payload = req.body.payloads[0];
+
+    var options = {
         vapidDetails: {
             subject: "mailto:remileguin@live.fr",
-            publicKey: req.body.vapidPublicKey,
-            privateKey: req.body.vapidPrivateKey,
+            publicKey: vapidPublicKey,
+            privateKey: vapidPrivateKey,
         },
         TTL: 60,
     };
-    var pushSubscription = {
-        endpoint: subscription.endpoint,
-        expirationTime: null,
-        keys: {
-            p256dh: subscription.p256dh,
-            auth: subscription.auth,
-        },
-    };
+
     webPush.sendNotification(pushSubscription, payload, options);
     res.status(201).send(JSON.stringify(pushSubscription) + ' - ' + JSON.stringify(payload));
 });
